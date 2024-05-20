d2242c2ac895022d2af51e7e13d6884c390af9f6
Aryan Agarwal <aryan23062001@gmail.com>
2024-05-20 18:03:58 +0530

    newupdate88

--- routes/deliveryAdressRoutes.js
+++ routes/deliveryAdressRoutes.js
@@ -5,7 +5,6 @@ import {
 } from "../controllers/deliveryController.js";
 import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
-
 router.route("/").post(protect, addDeliverAdress);
 router.route("/myadresses").get(protect, getMyAdresses);
 export default router;
